727d8fa9c1653e4de4d538c50906250c430ffc8c
Helanka Singhapurage <helankas26@gmail.com>
2023-03-31 18:04:17 +0530

    implement AdminDashboard

--- src/com/chatapp/controller/AdminDashboardFormController.java
+++ src/com/chatapp/controller/AdminDashboardFormController.java
@@ -4,11 +4,14 @@
  */
 package com.chatapp.controller;
 
+import com.chatapp.util.Cookie;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
@@ -18,11 +21,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -60,6 +67,11 @@ public class AdminDashboardFormController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        try {
+            checkUser();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
         initializeDate();
         initializeTime();
         vUser.setVisible(false); 
@@ -94,7 +106,23 @@ public class AdminDashboardFormController implements Initializable {
 
     @FXML
     private void logoutOnAction(ActionEvent event) throws IOException {
-        setUi("LoginForm");
+        Optional<ButtonType> result = alert(
+                Alert.AlertType.CONFIRMATION,
+                "Logout",
+                "Do you really want to logout?"
+        );
+                
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            try {
+                Cookie.setLoginUser(null);
+                setUi("LoginForm");
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+        } else {
+            event.consume();
+        }
     }
 
     @FXML
@@ -102,12 +130,10 @@ public class AdminDashboardFormController implements Initializable {
         loadUi("ViewUserForm");
     }
 
-
     @FXML
     private void viewChatOnAction(ActionEvent event) throws IOException {
         loadUi("ViewChatForm");
     }
-
 
     @FXML
     private void editChatOnAction(ActionEvent event) throws IOException {
@@ -130,8 +156,11 @@ public class AdminDashboardFormController implements Initializable {
     }
     
     private void setUi(String location) throws IOException {
-        Stage stage = (Stage) loginContext.getScene().getWindow();
+        Stage stage = (Stage) adminDashboardContext.getScene().getWindow();
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"))));
+        stage.setOnCloseRequest((event) -> {
+            System.exit(0);
+        });
     }
     
     private void loadUi(String location) throws IOException {      
@@ -140,7 +169,7 @@ public class AdminDashboardFormController implements Initializable {
         adminMainContext.getChildren().add(loder.load());
     }
     
-    public void initializeDate() {
+    private void initializeDate() {
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.seconds(0),
                         e -> {
@@ -156,7 +185,7 @@ public class AdminDashboardFormController implements Initializable {
         timeline.play();      
     }
      
-    public void initializeTime() {
+    private void initializeTime() {
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.seconds(0),
                         e -> {
@@ -170,5 +199,33 @@ public class AdminDashboardFormController implements Initializable {
 
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
+    }
+    
+    private void checkUser() throws IOException {
+        if (null == Cookie.getLoginUser()) {
+            setUi("LoginForm");
+        }
+    }
+    
+    private Optional<ButtonType> alert(Alert.AlertType alertType, String title, String contentText) {
+        Optional<ButtonType> result = null;
+        
+        try {
+            Stage parentStage = (Stage) adminDashboardContext.getScene().getWindow();
+            Alert alert = new Alert(alertType);
+            Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+            String iconPath = getClass().getResource("../asserts/view_chat.png").toURI().toString();
+            stage.getIcons().add(new Image(iconPath));
+            alert.initModality(Modality.APPLICATION_MODAL);
+            alert.initOwner(parentStage);
+            alert.setTitle(title);
+            alert.setHeaderText(null);
+            alert.setContentText(contentText);
+            
+            result = alert.showAndWait();
+        } catch (URISyntaxException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return result;
     }
 }
